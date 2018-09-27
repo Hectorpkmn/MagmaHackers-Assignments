02874eee373dc55fd82159ed760b8a8b24564ca2
Hectorpkmn <hector.pkmn@gmail.com>
2018-09-27 11:40:05 -0500

    Breaking into separate files and and logic updated

---
+++ Tic-Tac-Toe/code/players.rb
@@ -0,0 +1,51 @@
+class Players
+  def player_1_moves()
+    puts "\n\nTurn of the player ONE\n"
+    print "please choose a free slot by typing its number: "
+    p1NumberChosen = gets.to_i
+    invalidCounter = 0
+    until (p1NumberChosen != 0) && (p1NumberChosen <= 9)
+      if invalidCounter == 5
+        p1NumberChosen = 0
+        break
+      else
+      puts "\nThat's not a valid input."
+      invalidCounter += 1
+      puts "invalidCounter: #{invalidCounter}"
+      print "\nplease type a valid number: "
+      p1NumberChosen = gets.to_i
+      end
+    end
+    puts "Your selected slot number was: #{p1NumberChosen}"
+    p1NumberChosen
+  end
+
+  def player_1_envia_coodernada
+    coordinate_to_send = p1NumberChosen
+    puts "El valor a enviar es: #{coordinate_to_send}"
+    coordinate_to_send
+  end
+
+=begin
+  def player_2_moves()
+    puts "\n\nTurn of the player TWO\n"
+    print "please choose a free slot by typing its number: "
+    p2NumberChosen = gets.to_i
+    invalidCounter = 0
+    until (p2NumberChosen != 0) && (p2NumberChosen <= 9)
+      if invalidCounter == 5
+        p2NumberChosen = 0
+        break
+      else
+      puts "\nThat's not a valid input."
+      invalidCounter += 1
+      puts "invalidCounter: #{invalidCounter}"
+      print "\nplease type a valid number: "
+      p2NumberChosen = gets.to_i
+      end
+    end
+    puts "Your selected slot number was: #{p2NumberChosen}"
+    return p2NumberChosen
+  end
+=end
+end
---
+++ Tic-Tac-Toe/code/tictactoe.rb
@@ -0,0 +1,32 @@
+require_relative 'grid'
+require_relative 'players'
+
+class TicTacToe
+  def initialize
+    @tablero = Grid.new
+    @jugador_x = Players.new
+#    @respuesta = Players.player_1_envia_coodernada
+  end
+
+  def greet_players
+    first_game = "This is the first game"
+    p1_turn = "Turn of the player 1"
+
+    puts "Welcome to Tic Tac Toe game"
+    @last_game_result = first_game
+    @player_turn = p1_turn
+    puts "#{@last_game_result} and it is #{@player_turn} \n"
+    puts "\nGame starts"
+  end
+
+  def start_game
+    greet_players
+    @tablero.create_grid
+    @tablero.draw_grid
+    #Hardcoding next steps by now for testing purpose
+    @jugador_x.player_1_moves
+    @tablero.receive_choice(4)
+    @tablero.draw_grid
+#    @jugadores.player_2_moves
+  end
+end
